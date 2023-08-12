e784092261ec8a3f696d64f6b32a0e38c6ee54f1
DylanHuang566 <135844898+DylanHuang566@users.noreply.github.com>
2023-08-12 07:58:52 +0000

    modify

--- taiwan_weather/main.py
+++ taiwan_weather/main.py
@@ -78,10 +78,14 @@ if not check_file_exist():
 
 file_path = get_fileName_path()
 dataFrame = pd.read_csv(file_path)
+dataFrame['啟始時間'] = pd.to_datetime(dataFrame['啟始時間'])
+dataFrame['結束時間'] = pd.to_datetime(dataFrame['結束時間'])
+dataFrame['啟始時間'] = dataFrame['啟始時間'].dt.strftime('%Y-%m-%d日-%H點')
+dataFrame['結束時間'] = dataFrame['結束時間'].dt.strftime('%Y-%m-%d日-%H點')
 #顯示標題
 st.title("台灣個縣市氣候:")
 st.subheader("攝氐")
 #顯示DataFrame
 st.dataframe(dataFrame,width=800,height=900)
-st.line_chart(dataFrame,x='城市',y=['最高溫度','最低溫度','感覺'])
-st.bar_chart(dataFrame,x='城市',y=['最高溫度','最低溫度'])
+st.line_chart(dataFrame,x='城市',y=['最高溫度','最低溫度'])
+#st.bar_chart(dataFrame,x='城市',y=['最高溫度','最低溫度'])
